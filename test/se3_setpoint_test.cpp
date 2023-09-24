ec089365b7b3ee5ec968ac2a131bd481e55105de
Mohamed Abdelkader <mohamedashraf123@gmail.com>
2023-09-24 04:53:20 +0300

    Add yaw param in test/se3_setpoint_test.cpp

--- test/se3_setpoint_test.cpp
+++ test/se3_setpoint_test.cpp
@@ -18,6 +18,9 @@ public:
         this->declare_parameter("z", 0.0f);
         z_ = this->get_parameter("z").get_parameter_value().get<float>();
 
+        this->declare_parameter("yaw", 0.0f);
+        yaw_ = this->get_parameter("yaw").get_parameter_value().get<float>();
+
         publisher_ = this->create_publisher<geometric_controller_ros::msg::TargetCommand>("se3controller/setpoint", 10);
         timer_ = this->create_wall_timer(20ms, std::bind(&TargetCommandPublisherNode::publishMessage, this));
     }
@@ -40,7 +43,7 @@ private:
         msg->jerk.x = 0.0;
         msg->jerk.y = 0.0;
         msg->jerk.z = 0.0;
-        msg->yaw = 0.0;
+        msg->yaw = yaw_;
         msg->yaw_dot = 0.0;
         msg->kx = {0.0, 0.0, 0.0};
         msg->kv = {0.0, 0.0, 0.0};
@@ -50,7 +53,7 @@ private:
     }
 
     // target positions
-    float x_, y_, z_;
+    float x_, y_, z_, yaw_;
 
     rclcpp::Publisher<geometric_controller_ros::msg::TargetCommand>::SharedPtr publisher_;
     rclcpp::TimerBase::SharedPtr timer_;
