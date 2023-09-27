f55f30d796571955d845936e9da09e75f3af304e
Mohamed Abdelkader <mohamedashraf123@gmail.com>
2023-09-27 12:59:22 +0300

    Impreove implementation of GeometricAttitudeControl

--- include/geometric_controller_ros/GeometricAttitudeControl.h
+++ include/geometric_controller_ros/GeometricAttitudeControl.h
@@ -19,29 +19,34 @@ class GeometricAttitudeControl
   void setCurrentOrientation(const Eigen::Quaternionf &current_orientation);
   void resetIntegrals();
   void setMaxTiltAngle(const float max_tilt_angle);
-  void setDragCoeff(const Eigen::Vector3f &drag);
-
-  void calculateControl(const Eigen::Vector3f &des_pos, const Eigen::Vector3f &des_vel, const Eigen::Vector3f &des_acc,
-                        const Eigen::Vector3f &des_jerk, const float des_yaw, const float des_yaw_dot,
-                        const Eigen::Vector3f &kx, const Eigen::Vector3f &kv, const Eigen::Vector3f &ki,
-                        const Eigen::Vector3f &ki_b);
 
   const Eigen::Vector3f &getComputedForce();
   const Eigen::Quaternionf &getComputedOrientation();
   const Eigen::Vector3f &getComputedAngularVelocity();
 
+  void calculateControl(const Eigen::Vector3f &des_pos, const Eigen::Vector3f &des_vel, const Eigen::Vector3f &des_acc,
+                        const Eigen::Vector3f &des_jerk, const float des_yaw, const float des_yaw_dot,
+                        const Eigen::Vector3f &kx, const Eigen::Vector3f &kv, const Eigen::Vector3f &ki,
+                        const Eigen::Vector3f &ki_b,
+                        const Eigen::Vector3f &kd, const float &attctrl_tau);
+
 private:
 
   Eigen::Vector3f controlPosition(const Eigen::Vector3f &target_pos, const Eigen::Vector3f &target_vel,
-                                  const Eigen::Vector3f &target_acc,
-                                  const Eigen::Vector3f &kx, const Eigen::Vector3f &kv);
-  void computeBodyRateCmd(Eigen::Vector4f &bodyrate_cmd, const Eigen::Vector3f &a_des) ;
-  Eigen::Vector4f acc2quaternion(const Eigen::Vector3f &vector_acc, const double &yaw);
+                                  const Eigen::Vector3f &target_acc, const float &des_yaw,
+                                  const Eigen::Vector3f &kx, const Eigen::Vector3f &kv, const Eigen::Vector3f &ki, 
+                                  const Eigen::Vector3f &kib, const Eigen::Vector3f &kd);
+  void computeBodyRateCmd(const Eigen::Vector3f &a_des, const float &des_yaw, const float &attctrl_tau) ;
+  Eigen::Vector4f acc2quaternion(const Eigen::Vector3f &vector_acc, const float &yaw);
   Eigen::Vector3f poscontroller(const Eigen::Vector3f &pos_error, const Eigen::Vector3f &vel_error, 
-                                const Eigen::Vector3f &kx, const Eigen::Vector3f &kv);
+                                const Eigen::Vector3f &kx, const Eigen::Vector3f &kv,
+                                const Eigen::Vector3f &ki, 
+                                  const Eigen::Vector3f &kib);
 
-  Eigen::Matrix3f quat2RotMatrix(const Eigen::Vector4f &q);
-  Eigen::Vector4f rot2Quaternion(const Eigen::Matrix3f &R);
+  static Eigen::Matrix3f quat2RotMatrix(const Eigen::Vector4f &q);
+  static Eigen::Vector4f rot2Quaternion(const Eigen::Matrix3f &R);
+  static Eigen::Matrix3f matrix_hat(const Eigen::Vector3f &v);
+  static Eigen::Vector3f matrix_hat_inv(const Eigen::Matrix3f &m);
 
   // Inputs for the controller
   float mass_;
@@ -51,7 +56,8 @@ private:
   Eigen::Vector3f vel_;
   float max_pos_int_;
   float max_pos_int_b_;
-  Eigen::Quaternionf current_orientation_;
+  Eigen::Quaternionf current_orientation_q_;
+  Eigen::Vector4f current_orientation_vec_;
   float cos_max_tilt_angle_;
   float max_accel_; // Maximum acceleration in m/s^2
 
@@ -64,9 +70,6 @@ private:
 
   // If true, yaw will be computed internally
   bool velocity_yaw_;
-  float mavYaw_;
-
-  Eigen::Vector3f D_;
 };
 
 #endif
